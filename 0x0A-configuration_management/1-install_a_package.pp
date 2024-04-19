1c64c196055682d5a179ea7bde6510bfc32af5d0
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-19 12:32:49 +0100

    Working on edge cases

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,10 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
+  command   => '/usr/bin/pip3 install Flask==2.1.0',
+  path      => ['/usr/bin'],
+  creates   => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
+  unless    => 'test -f /usr/local/lib/python3.8/dist-packages/flask/__init__.py && /usr/bin/flask --version | grep -q "Flask 2.1.0"',
+  timeout   => 300,  # Set a timeout for the execution
+  logoutput => true,  # Log the output of the execution
 }
