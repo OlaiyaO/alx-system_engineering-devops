42daaa0f79554713c476e8f79c665fe81699c6ea
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-19 13:09:13 +0100

    made some minor edits

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-# This Puppet manifest installs Flask package using pip3
+# This Puppet manifest installs Flask package using pip
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip',
 }
