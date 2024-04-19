e40bbb17e9dc1e34674dfb7eee868fa6b8c774f4
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-19 17:38:54 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# This Puppet manifest installs Flask package using pip
-
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3'
 }
