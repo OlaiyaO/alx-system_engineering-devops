8b06ca417380ae26fd8bb13e844430ad7118e8c4
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-19 12:13:48 +0100

    Did something different

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This Puppet manifest installs Flask package using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
 }
