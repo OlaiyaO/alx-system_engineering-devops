9a66b86a73dc51f9a1dbf31f912b3e0dbb4f8a92
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-06-05 00:05:02 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+number of subscribers for a given subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/JB)'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
+    try:
+        response = requests.get(url, headers=user_agent, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+
+        results = response.json()
+        return results.get('data', {}).get('subscribers', 0)
+    
+    except requests.RequestException:
+        return 0
+    except ValueError:  # JSON decoding failed
         return 0
