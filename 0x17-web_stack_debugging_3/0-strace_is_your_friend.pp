d7b90dd0a50009ac67b08fb3716c017bed26c276
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-06-04 13:15:10 +0100

    0x17-web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix Server side 500 error when a GET HTTP method is requested to Apache
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
